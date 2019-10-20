7c59e2b1f55df2f6f1b2374568849b611878b66e
bashard001 <bdaseh@gmail.com>
2019-10-20 11:55:37 -0700

    fixed saving in local storage issues

--- stylesheets/dayCalendar.js
+++ stylesheets/dayCalendar.js
@@ -2,18 +2,15 @@ var date = moment().format('LL')
 var dateContent = $("<h3>" + date + "</h3>")
 dateContent.addClass("title")
 $("#main-content").append(dateContent)
+var textArea = $("textarea")
+
 
 
 
 $("button").on("click", function(){
     var targetBtn = $(this).attr("time")
-    var caContent = $("textarea")
-    for (var i = 0; caContent.length; i++){
-        if (targetBtn == caContent.attr("time")){
-            var letsGo = caContent.val()
-            console.log(letsGo)
-            break
-        }
-    }
+    var caContent = $("#textarea" + $(this).attr("time"))
 
+            var letsGo = caContent.val()
+            localStorage.setItem(caContent.attr("id"), letsGo)
 })
