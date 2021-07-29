59b7da43c782cd0dd0e344d7e14cfe870bbeb59c
bashard001 <bdaseh@gmail.com>
2021-07-28 22:19:33 -0700

    updated currentHour var and the page title

--- dayCalendar.js
+++ dayCalendar.js
@@ -1,24 +1,20 @@
 $(function(){
 
 var date = moment().format('LL')
-
+var currentHour = new Date().getHours()
 var dateContent = $("<h4>" + date + "</h4>")
 dateContent.addClass("title")
-$("#main-content").append(dateContent)
 
 var liveHour = moment().format('LT')
 var liveHourEl = $("<h4 id='timeh4' class='title' style='padding-top:0px'>" + liveHour + "<h4>")
 var textArea = $("textarea")
-$("#main-content").append(liveHourEl)
+
 
 var buttons = $("button")
 buttons.addClass("btnColor")
 
-var today = new Date()
-
-var currentHour = today.getHours();
-console.log(currentHour)
-console.log(today)
+$("#main-content").append(liveHourEl)
+$("#main-content").append(dateContent)
 
 function currentStatus(){
     textArea.each(function(){
@@ -47,7 +43,7 @@ for (var i = 0; i < textArea.length; i++){
 
 // takes the info from the textarea and save it to the localStorage
 $("button").on("click", function(){
-    var targetBtn = $(this).attr("time")
+
     var caContent = $("#textarea" + $(this).attr("time"))
 
             var letsGo = caContent.val()
