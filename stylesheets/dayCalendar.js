9dbb5b49a2655234749c53bb3292fe7ae0665981
bashard001 <bdaseh@gmail.com>
2021-07-28 21:45:51 -0700

    update the code

--- stylesheets/dayCalendar.js
+++ stylesheets/dayCalendar.js
@@ -1,7 +1,11 @@
+$(function(){
+
 var date = moment().format('LL')
+
 var dateContent = $("<h4>" + date + "</h4>")
 dateContent.addClass("title")
 $("#main-content").append(dateContent)
+
 var liveHour = moment().format('LT')
 var liveHourEl = $("<h4 id='timeh4' class='title' style='padding-top:0px'>" + liveHour + "<h4>")
 var textArea = $("textarea")
@@ -11,12 +15,12 @@ var buttons = $("button")
 buttons.addClass("btnColor")
 
 var today = new Date()
+
 var currentHour = today.getHours();
 console.log(currentHour)
 console.log(today)
 
-function  currentStatus(){
-
+function currentStatus(){
     textArea.each(function(){
         if ($(this).attr("time") > currentHour){
             $(this).attr("class", "green")
@@ -52,3 +56,4 @@ $("button").on("click", function(){
 
 fillInCalender()
 currentStatus()
+})
