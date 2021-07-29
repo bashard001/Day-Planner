ec737b1364f7c0679cb20de10acd8a3f793ee315
bashard001 <bdaseh@gmail.com>
2021-07-29 13:31:43 -0700

    created a createCalendar function that dynamically renders calendar components to the dom using jquery

--- dayCalendar.js
+++ dayCalendar.js
@@ -7,8 +7,9 @@ dateContent.addClass("title")
 
 var liveHour = moment().format('LT')
 var liveHourEl = $("<h4 id='timeh4' class='title' style='padding-top:0px'>" + liveHour + "<h4>")
-var textArea = $("textarea")
 
+var textArea
+var workingHours = ["9am","10am","11am","12am","1pm","2pm","3pm","4pm","5pm"]
 
 var buttons = $("button")
 buttons.addClass("btnColor")
@@ -17,6 +18,7 @@ $("#main-content").append(liveHourEl)
 $("#main-content").append(dateContent)
 
 function currentStatus(){
+     textArea = $("textarea")
     textArea.each(function(){
         if ($(this).attr("time") > currentHour){
             $(this).attr("class", "green")
@@ -24,14 +26,32 @@ function currentStatus(){
             $(this).attr("class", "red")
         } else {
             $(this).attr("class", "yellow")
-
         }
     })
-
 }
 
+var createCalendar = new Promise ((resolve, reject) => {
+    let i = 9
+    resolve(  
+        
+    workingHours.forEach(el =>{
+        
+        $(".container").append(`<div class='hour'>${el}</div>`)
+        $(".container").append(`<textarea time=${i}
+         id='textarea${el.length > 3 ? el[0]+el[1] : el[0]}'></textarea>`)
+        $(".container").append(`<button time=${el.length > 3 ? el[0]+el[1] : el[0]}>Save</button>`)
+        i++
+    } ))
+})
+    
+
+
+
+
 // inserts the information from the local storage to the text area
-function fillInCalender(){
+async function fillInCalendar(){
+    await createCalendar.then(currentStatus())
+    console.log(textArea.length)
 for (var i = 0; i < textArea.length; i++){
     var itemName = textArea[i].getAttribute("id")
     var getContent = localStorage.getItem(itemName)
@@ -50,6 +70,7 @@ $("button").on("click", function(){
             localStorage.setItem(caContent.attr("id"), letsGo)
 })
 
-fillInCalender()
-currentStatus()
+
+fillInCalendar()
+
 })
