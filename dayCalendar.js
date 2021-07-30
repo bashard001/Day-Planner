aa3bbfdd93041a678a80f808351efb92ffae5049
unknown <bdaseh@gmail.com>
2021-07-29 19:58:57 -0700

    updated styles

--- dayCalendar.js
+++ dayCalendar.js
@@ -11,8 +11,6 @@ var liveHourEl = $("<h4 id='timeh4' class='title' style='padding-top:0px'>" + li
 var textArea
 var workingHours = ["9am","10am","11am","12am","1pm","2pm","3pm","4pm","5pm"]
 
-var buttons = $("button")
-buttons.addClass("btnColor")
 
 $("#main-content").append(liveHourEl)
 $("#main-content").append(dateContent)
@@ -32,21 +30,18 @@ function currentStatus(){
 
 var createCalendar = new Promise ((resolve, reject) => {
     let i = 9
+
     resolve(  
-        
     workingHours.forEach(el =>{
         
         $(".container").append(`<div class='hour'>${el}</div>`)
         $(".container").append(`<textarea time=${i}
          id='textarea${el.length > 3 ? el[0]+el[1] : el[0]}'></textarea>`)
-        $(".container").append(`<button time=${el.length > 3 ? el[0]+el[1] : el[0]}>Save</button>`)
+        $(".container").append(`<button class="btnColor" time=${el.length > 3 ? el[0]+el[1] : el[0]}>Save</button>`)
         i++
     } ))
 })
     
-
-
-
 
 // inserts the information from the local storage to the text area
 async function fillInCalendar(){
@@ -56,9 +51,7 @@ for (var i = 0; i < textArea.length; i++){
     var itemName = textArea[i].getAttribute("id")
     var getContent = localStorage.getItem(itemName)
     textArea[i].textContent = getContent
-}
-
-}
+}}
 
 
 // takes the info from the textarea and save it to the localStorage
