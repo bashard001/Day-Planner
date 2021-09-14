c8a089033527ac3955a220761f0b135692d0d8b3
bashard001 <bdaseh@gmail.com>
2021-09-13 19:08:16 -0700

    saving errands to local storage

--- dayCalendar.js
+++ dayCalendar.js
@@ -3,17 +3,18 @@ $(function () {
     var date = moment().format('LL')
     var currentHour = new Date().getHours()
     var dateContent = $("<h4>" + date + "</h4>")
-
     var liveHour = moment().format('LT')
     var liveHourEl = $("<h4 id='timeh4'>" + liveHour + "</h4>")
     var workingHours = ["9am", "10am", "11am", "12am", "1pm", "2pm", "3pm", "4pm", "5pm"]
-
+    var textArea
+    
     $("#main-content").append(dateContent)
     $("#main-content").append(liveHourEl)
 
 
-    function currentStatus() {
-        var textArea = $("textarea")
+  async function currentStatus() {
+         textArea = $("textarea")
+
         textArea.each(function () {
             if ($(this).attr("time") > currentHour) {
                 $(this).attr("class", "green")
@@ -39,23 +40,22 @@ $(function () {
             }))
     })
 
-
     // inserts the information from the local storage to the text area
     async function fillInCalendar() {
         await createCalendar.then(currentStatus())
-        console.log(textArea.length)
-        for (var i = 0; i < textArea.length; i++) {
+
+        for (let i = 0; i < textArea.length; i++) {
             var itemName = textArea[i].getAttribute("id")
             var getContent = localStorage.getItem(itemName)
             textArea[i].textContent = getContent
         }
+        
     }
 
     // takes the info from the textarea and save it to the localStorage
     $("button").on("click", function () {
 
         var caContent = $("#textarea" + $(this).attr("time"))
-
         var letsGo = caContent.val()
         localStorage.setItem(caContent.attr("id"), letsGo)
         alert("Saved")
@@ -63,3 +63,6 @@ $(function () {
 
     fillInCalendar()
 })
+
+
+
